da35c608d79d88992d36778702a1e74b50652599
d0uwhs <d0uwhs@gmail.com>
2022-08-25 17:11:24 +0900

    DAY - 01

---
+++ src/dayone/1_1_if.js
@@ -0,0 +1,17 @@
+/**
+ * 1. IF
+ */
+
+const grade = '운영자';
+if (grade === '손님'){
+    const authority = '글 읽기';
+    console.log(`${grade}의 경우 ${authority}가 가능합니다.`)
+}
+if (grade === '일반 회원'){
+    const authority = '글 읽기, 글 쓰기';
+    console.log(`${grade}의 경우 ${authority}가 가능합니다.`)
+}
+if (grade === '운영자') {
+    const authority = '글 읽기, 글 쓰기, 글 삭제';
+    console.log(`${grade}의 경우 ${authority}가 가능합니다.`)
+}
