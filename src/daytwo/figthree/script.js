abe3cbc5f918ae884e16df757f8f19bfa1acaf43
d0uwhs <d0uwhs@gmail.com>
2022-08-31 01:56:01 +0900

    DAY - 04

--- src/daytwo/figthree/script.js
+++ src/daytwo/figthree/script.js
@@ -1,1 +1,16 @@
 "use strict";
+const tagValue = document.querySelector('input');
+const idValue = document.querySelector('#pname');
+const nameValue = document.getElementsByName('price')[0];
+const printValue = () => {
+    const target = document.querySelector('#area');
+    const buyerTag = document.createElement('div');
+    buyerTag.innerHTML = `구매자 : ${idValue.value}`;
+    const productTag = document.createElement('div');
+    productTag.innerHTML = `상품명 : ${nameValue.value}`;
+    const priceTag = document.createElement('div');
+    priceTag.innerHTML = `가격 : ${tagValue.value}`;
+    target === null || target === void 0 ? void 0 : target.appendChild(buyerTag);
+    target === null || target === void 0 ? void 0 : target.appendChild(productTag);
+    target === null || target === void 0 ? void 0 : target.appendChild(priceTag);
+};
--- src/daytwo/figtwo/script.js
+++ src/daytwo/figtwo/script.js
@@ -4,13 +4,13 @@ const selectColor = document.querySelector('#selectColor');
 const handleOnClickForChangeColor = () => {
     boxOne.setAttribute('style', `background-color : ${selectColor.value}`);
 };
-const boxTwo = document.querySelector('#box2');
+const divElement = document.querySelector('#box2');
 const setHalfSize = () => {
-    boxTwo.setAttribute('style', 'width : 50px; height : 50px');
+    divElement.setAttribute('style', 'width : 50px; height : 50px');
 };
 const setDefaultSize = () => {
-    boxTwo.setAttribute('style', 'width : 100px; height : 100px');
+    divElement.setAttribute('style', 'width : 100px; height : 100px');
 };
 const setDoubleSize = () => {
-    boxTwo.setAttribute('style', 'width : 200px; height : 200px');
+    divElement.setAttribute('style', 'width : 200px; height : 200px');
 };
